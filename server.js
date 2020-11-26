18457511d3f1ac733af191fc6bf3dbd313e8b94c
bguilas <bryanjguilas@gmail.com>
2020-11-26 11:03:20 -0800

    added mongoDb connection

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const PORT = process.env.PORT || 5000;
-
 const app = express();
 
+//Middleware
 app.use(cors());
+require('dotenv').config();
+
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Serve up static assets (For heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+//Mongoose Connection
+const uri = process.env.mongoose.connection;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established');
+});
+
+//ROUTES GO HERE
 
 
-
-
-
+//Express Server
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT} at http://localhost:${PORT}`)
-})
+});
