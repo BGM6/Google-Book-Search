d8238e6fe7edc2233c52ecec74cb89616774b03d
bguilas <bryanjguilas@gmail.com>
2020-11-26 11:16:02 -0800

    created books schema

---
+++ models/books.js
@@ -0,0 +1,16 @@
+const { Schema, model} = require('mongoose');
+
+const bookSchema = new Schema({
+    title: { type: String, required: true },
+    authors: { type: String, required: true },
+    description: { type: String, required: true },
+    link: {type: String, required: true},
+    image: {type: String, required: true},
+    googleId: { type: String, required: true, unique: true },
+
+})
+
+
+const Books = model('Books', bookSchema);
+
+module.exports = Books;
