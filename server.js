64626b764b84136f41f45602e55a94cb020df26e
bguilas <bryanjguilas@gmail.com>
2020-11-26 10:55:24 -0800

    added server and .env

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT} at http://localhost:${PORT}`)
+})
