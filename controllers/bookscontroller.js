af0148a41d50b925b4f836b333d792a38e8c36ce
bguilas <bryanjguilas@gmail.com>
2020-12-05 13:13:52 -0800

    resloved issue with backend

--- controllers/bookscontroller.js
+++ controllers/bookscontroller.js
@@ -1,6 +1,3 @@
-//This allows the developer to require a whole directory
-//In this instance I can require the whole models folder with this index.js export
-
 const db = require("../models");
 
 module.exports = {
